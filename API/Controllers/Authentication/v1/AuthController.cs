cf9798168be20525143251aa71000aaf89f9ef49
A.Sami <wow1.devill@gmail.com>
2022-08-16 16:43:40 +0430

    Health Check

--- API/Controllers/Authentication/v1/AuthController.cs
+++ API/Controllers/Authentication/v1/AuthController.cs
@@ -13,14 +13,28 @@ namespace Caffe.API.Controllers.Authentication.v1
     {
         private readonly ISerilogService _serilog;
         private readonly IAuthService _authService;
-        public AuthController(ISerilogService serilog, IAuthService authService, IIdentityService identityService, IDateTime date, IJwtTokenGenerator jwt)
+        private readonly IWebHostEnvironment _env;
+
+        public AuthController(ISerilogService serilog, IAuthService authService, IIdentityService identityService, IDateTime date, IJwtTokenGenerator jwt, IWebHostEnvironment env)
         {
             _serilog = serilog ?? throw new ArgumentNullException(nameof(serilog));
             _authService = authService ?? throw new ArgumentNullException(nameof(authService));
+            _env = env ?? throw new ArgumentNullException(nameof(env));
         }
 
         /// <summary>
-        /// 
+        /// Endpoint to health check the api and environment name
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("HealthCheck")]
+        public IActionResult HealthCheck()
+        {
+            //check the webhost environment
+            return Ok(_env.EnvironmentName);
+        }
+
+        /// <summary>
+        ///
         /// </summary>
         /// <param name="req"></param>
         /// <returns></returns>
@@ -45,6 +59,6 @@ namespace Caffe.API.Controllers.Authentication.v1
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
-        
+
     }
 }
